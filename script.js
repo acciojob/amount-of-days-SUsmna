24aaf1f2a7e003f082115ba035201c9b3ffd3fe2
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-05-15 11:34:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function daysofYears(year){
+	if((year%4===0 && year%100 !==0)||(year%400===0)){
+		return 366;
+	}else{
+		return 365;
+	}
+}
+
+console.log(daysofYears(2022));
+console.log(daysofYears(2024));
+console.log(daysofYears(1900));
+console.log(daysofYears(2000));
